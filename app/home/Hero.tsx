06d60597d267615c2eb67f01e57362fa888c55e5
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-06-26 20:50:26 +0200

    basic `Hero` component added

---
+++ app/home/Hero.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+import React from 'react';
+
+import Button from '../components/Button';
+
+const Hero = () => {
+  const handleClick = () => {};
+
+  return (
+    <section>
+      <h2>Lorem ipsum dolor si amet</h2>
+      <Button className="" onClick={handleClick}>
+        Umów wizytę
+      </Button>
+    </section>
+  );
+};
+
+export default Hero;
