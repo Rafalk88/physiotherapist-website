106a2a38ba8013e679a7ca3ccb13481049d8e40d
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-06-27 23:31:41 +0200

    `Button` added variants and colors

--- app/components/Button/index.tsx
+++ app/components/Button/index.tsx
@@ -4,16 +4,45 @@ import React from 'react';
 
 type Props = {
   className?: string
+  variant: React.ReactNode
+  color: React.ReactNode
   children: React.ReactNode
   onClick: () => void
 };
 
+const variantClasses = {
+  contained: 'rounded-full',
+  outlined: 'rounded-full border',
+  text: 'underline',
+};
+
+const colorClasses = {
+  primary: 'bg-orange text-dark-blue',
+  secondary: 'border-orange text-dark-blue',
+  link: '',
+};
+
 const Button = ({
-  className = '', children, onClick, ...props
-}: Props) => (
-  <button type="button" className={className} onClick={onClick} {...props}>
-    {children}
-  </button>
-);
+  className = '',
+  variant,
+  color,
+  children,
+  onClick,
+  ...props
+}: Props) => {
+  const variantStyles = variantClasses[variant];
+  const colorStyles = colorClasses[color];
+
+  return (
+    <button
+      className={`${variantStyles} ${colorStyles} ${className}`}
+      type="button"
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default Button;
