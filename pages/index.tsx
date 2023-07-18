29aaf1c0a404225ff60fe26c9aa1ce7ed3741d96
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-07-18 20:23:10 +0200

    refactor(index): airbnb rules and SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,13 @@ import Head from 'next/head';
 import React from 'react';
 import Landing from './landing';
 
-const Home = () => {
-  
-  return (
-    <>
-      <Head>
-        <title>Siltim Sp. z o.o. | Strona główna</title>
-      </Head>
-      <Landing />
-    </>
-  )
-};
+const Home = () => (
+  <>
+    <Head>
+      <title>Forwell | Strona główna</title>
+    </Head>
+    <Landing />
+  </>
+);
 
 export default Home;
