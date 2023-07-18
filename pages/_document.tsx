837cd6aed12b12e840047d4c71c6187602d9e310
Rafal Kochanecki <rafal.kochanecki@gmail.com>
2023-07-18 20:22:27 +0200

    refactor(document): airbnb rules and SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,36 +1,39 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import React from 'react';
+import {
+  Html, Head, Main, NextScript,
+} from 'next/document';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>    
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#2d89ef" />
-        <meta name="theme-color" content="#ffffff" />
-        <meta name="keywords" content="Siltim, benzen, reakcja chemiczna, chemia, labolatorium"></meta>
-        <meta name="author" content="Rafał Kochanecki"></meta>
-        <meta
-          name="description"
-          content="Strona firmy chemicznej wraz ze sklepem z materiami. Jesteśmy czynni od poniedziałku do soboty od 8:00 do 18:00"
-        />
-        <meta property="og:title" content="Siltim sp. z o.o." />
-        <meta
-          property="og:description"
-          content="Advanced materials for molecular design"
-        />
-        <meta
-          property="og:image"
-          content="../public/assets/logo_black.png"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+export const Document = () => (
+  <Html lang="en">
+    <Head>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta name="keywords" content="Forwell, trening personalny, masaż, fizjoterapia" />
+      <meta name="author" content="Rafał Kochanecki" />
+      <meta
+        name="description"
+        content="Strona firmowa. Jesteśmy czynni od poniedziałku do soboty od 8:00 do 18:00"
+      />
+      <meta property="og:title" content="Forwell" />
+      <meta
+        property="og:description"
+        content="Centrum fizjoterapii, treningu personalnego i masażu"
+      />
+      <meta
+        property="og:image"
+        content="../public/assets/logo_black.png"
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
